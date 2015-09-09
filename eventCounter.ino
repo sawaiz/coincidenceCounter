c20fe734b16ac2531b3ff0f139ae1baf4f7b4b48
Sawaiz Syed <github@sawaizsyed.com>
2015-09-09 16:06:40 -0400

    Initaial commit

---
+++ eventCounter.ino
@@ -0,0 +1,37 @@
+int counts;
+int inputPin = 2;
+
+void setup()
+{
+  Serial.begin(9600);
+  //pinMode(inputPin, INPUT);
+  counts = 0;
+  attachInterrupt(0, blink, LOW);
+}
+
+void loop()
+{
+  Serial.println(counts);
+  counts = 0;
+  delay(1000);
+}
+
+void blink()
+{
+    detachInterrupt (0);
+    counts++;
+    //while(digitalRead(inputPin!=0)){
+      //Serial.println(digitalRead(inputPin));
+    //  }
+    delay(2);
+    attachInterrupt(0, blink, LOW);
+}
+
+
+
+
+
+
+
+
+
